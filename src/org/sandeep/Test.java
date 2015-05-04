83a4eafce3d2e67a9eb95a76ffaaac2c5b00212f
sandeep <p.sandeepkumargupta@gmail.com>
2015-05-04 23:50:03 +0530

    second commit

--- src/org/sandeep/Test.java
+++ src/org/sandeep/Test.java
@@ -2,7 +2,15 @@ package org.sandeep;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Sandeep Perumalla");
+		System.out.println("Hello Sandeep Perumalla");
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
 	}
+	
+	
+	
 
 }
